11cc492a069f0d5630eefba653a1d90c4ce19722
Dejan Miocinovic <dekimioc@yahoo.com>
2020-02-13 22:17:26 +0100

    First commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <div className="left-side">
+                <a>
+                    <img  />
+                </a>
+            </div>
+            <div className="right-side">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default Header;

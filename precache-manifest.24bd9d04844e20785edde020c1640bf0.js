64c97b6a87959f815d0fca301b4e3a3fa83bbfe7
Dejan Miocinovic <dekimioc@yahoo.com>
2020-03-02 22:43:25 +0100

    Updates

---
+++ precache-manifest.24bd9d04844e20785edde020c1640bf0.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccdf19894cb0827dba36b8795345d83",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "0c1c2d13febe08974c14",
+    "url": "/portfolio-2020/static/css/2.e016cb85.chunk.css"
+  },
+  {
+    "revision": "38d26a39ffba3c193104",
+    "url": "/portfolio-2020/static/css/main.0e5e24e3.chunk.css"
+  },
+  {
+    "revision": "0c1c2d13febe08974c14",
+    "url": "/portfolio-2020/static/js/2.567625c1.chunk.js"
+  },
+  {
+    "revision": "486c864dbeea4f82b0912c66e3826be4",
+    "url": "/portfolio-2020/static/js/2.567625c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d26a39ffba3c193104",
+    "url": "/portfolio-2020/static/js/main.a005faca.chunk.js"
+  },
+  {
+    "revision": "735fdea747ef26147412",
+    "url": "/portfolio-2020/static/js/runtime-main.d9909059.js"
+  },
+  {
+    "revision": "36ccfa02bc5482ef646fdd37957355bd",
+    "url": "/portfolio-2020/static/media/Bakson.36ccfa02.png"
+  },
+  {
+    "revision": "07dda4eab223bdc3a1dbe82f91b7f3f6",
+    "url": "/portfolio-2020/static/media/DM.07dda4ea.png"
+  },
+  {
+    "revision": "4717c6a0e3fe9fd7c1980b4c36e3da3f",
+    "url": "/portfolio-2020/static/media/aboutPage.4717c6a0.jpg"
+  },
+  {
+    "revision": "0920f6b05339ff39eb86268cf72af2b6",
+    "url": "/portfolio-2020/static/media/bakson-small.0920f6b0.jpg"
+  },
+  {
+    "revision": "b8370cd3ee7495183d7674f09b54416f",
+    "url": "/portfolio-2020/static/media/bakson.b8370cd3.jpg"
+  },
+  {
+    "revision": "5361775e7505cda86b0cb26e1fa65465",
+    "url": "/portfolio-2020/static/media/bakson2.5361775e.jpg"
+  },
+  {
+    "revision": "2bbbd14574af775e765c768ad8194aa8",
+    "url": "/portfolio-2020/static/media/burger-builder-small.2bbbd145.jpg"
+  },
+  {
+    "revision": "23e5eed0bb7dba56536f70ffd311b1b1",
+    "url": "/portfolio-2020/static/media/burger.23e5eed0.png"
+  },
+  {
+    "revision": "ffcccb47231a4cdb5ca92ca439e9b154",
+    "url": "/portfolio-2020/static/media/burger1.ffcccb47.jpg"
+  },
+  {
+    "revision": "40e1fb7749dc5caa3f4b3ab633adb942",
+    "url": "/portfolio-2020/static/media/burger2.40e1fb77.jpg"
+  },
+  {
+    "revision": "4a1fd0f4d1d9cc7e791f25c41433d2c1",
+    "url": "/portfolio-2020/static/media/chore-door-small.4a1fd0f4.jpg"
+  },
+  {
+    "revision": "0a4dc7a34a3eec02981ab235ca7c6c83",
+    "url": "/portfolio-2020/static/media/chore1.0a4dc7a3.jpg"
+  },
+  {
+    "revision": "ddcd8209592f62c362c44a639f9203c0",
+    "url": "/portfolio-2020/static/media/chore2.ddcd8209.jpg"
+  },
+  {
+    "revision": "4972db9c43473596b2f1ae749b3bb2dd",
+    "url": "/portfolio-2020/static/media/choreDoor.4972db9c.png"
+  },
+  {
+    "revision": "b19b4945e631ff5007dc741fe5441398",
+    "url": "/portfolio-2020/static/media/dice.b19b4945.png"
+  },
+  {
+    "revision": "e5399755221111a7dd8e4047131a6aa1",
+    "url": "/portfolio-2020/static/media/dice1.e5399755.jpg"
+  },
+  {
+    "revision": "d0f739e4efed90ae70cdec18fcd9fe36",
+    "url": "/portfolio-2020/static/media/dice2.d0f739e4.jpg"
+  },
+  {
+    "revision": "f4897561b88c5a10ec4c88f9354e1be6",
+    "url": "/portfolio-2020/static/media/e-commerce-react1.f4897561.jpg"
+  },
+  {
+    "revision": "31a21b7b6e485b565eedde65d1778242",
+    "url": "/portfolio-2020/static/media/e-commerce-react2.31a21b7b.jpg"
+  },
+  {
+    "revision": "124280b3f5ae53bf85e8bfa45079cf5a",
+    "url": "/portfolio-2020/static/media/ecommerce.124280b3.png"
+  },
+  {
+    "revision": "975b0b7697083c9c1ef9696dd97c42e7",
+    "url": "/portfolio-2020/static/media/hexa-small.975b0b76.jpg"
+  },
+  {
+    "revision": "b6c46dd77467d85927f7b0b1e8d759ba",
+    "url": "/portfolio-2020/static/media/hexa.b6c46dd7.png"
+  },
+  {
+    "revision": "ab9902f6d9a877b2b6a94761d74d9784",
+    "url": "/portfolio-2020/static/media/hexa1.ab9902f6.jpg"
+  },
+  {
+    "revision": "cc03af227a5b97d55ef350c18493ad63",
+    "url": "/portfolio-2020/static/media/hexa2.cc03af22.jpg"
+  },
+  {
+    "revision": "6d645875bbc9ab06178081af6c9c3b88",
+    "url": "/portfolio-2020/static/media/js-shop-small.6d645875.jpg"
+  },
+  {
+    "revision": "f948db809526cc3d0f85e2b4e5082199",
+    "url": "/portfolio-2020/static/media/miami-small.f948db80.jpg"
+  },
+  {
+    "revision": "fd46961c1e3a0fc8cdf6ea225f120fa1",
+    "url": "/portfolio-2020/static/media/miami.fd46961c.png"
+  },
+  {
+    "revision": "629b4fedff7866b8f5be3f6c8fc5976c",
+    "url": "/portfolio-2020/static/media/miami1.629b4fed.jpg"
+  },
+  {
+    "revision": "9d22604fdc03dffebf8b9cc5435cebc8",
+    "url": "/portfolio-2020/static/media/miami2.9d22604f.jpg"
+  },
+  {
+    "revision": "25ba75830da823c65e21a0831290f612",
+    "url": "/portfolio-2020/static/media/minishop.25ba7583.png"
+  },
+  {
+    "revision": "387dcf20804dba6b506d2fcfb0dd4ee8",
+    "url": "/portfolio-2020/static/media/minishop.387dcf20.jpg"
+  },
+  {
+    "revision": "0b7a14962a1ecc913a6caa194f617bb5",
+    "url": "/portfolio-2020/static/media/monster-attack.0b7a1496.jpg"
+  },
+  {
+    "revision": "d62abae62fcc1d90efa632c3d9cc7e1d",
+    "url": "/portfolio-2020/static/media/monster1.d62abae6.jpg"
+  },
+  {
+    "revision": "d232f2d8b371d2d25ef1a994fda8e7fa",
+    "url": "/portfolio-2020/static/media/monster2.d232f2d8.jpg"
+  },
+  {
+    "revision": "c94c4df4140c06dbc2d836a9d7b4a103",
+    "url": "/portfolio-2020/static/media/monsterattack1.c94c4df4.jpg"
+  },
+  {
+    "revision": "0d14d74f5b477574de463db8fc480668",
+    "url": "/portfolio-2020/static/media/monsterattack2.0d14d74f.jpg"
+  },
+  {
+    "revision": "bdf39f226f4eec2ebdb5a4f775dda489",
+    "url": "/portfolio-2020/static/media/monsters-card-small.bdf39f22.jpg"
+  },
+  {
+    "revision": "09cfe052365a93774cf735b88c825f48",
+    "url": "/portfolio-2020/static/media/monstersapp.09cfe052.png"
+  },
+  {
+    "revision": "bbd88ea463bb27d25654be51591f1ab7",
+    "url": "/portfolio-2020/static/media/monstersattack.bbd88ea4.png"
+  },
+  {
+    "revision": "19bccb52cdb2c3b36e0cc6864cd819c5",
+    "url": "/portfolio-2020/static/media/myImage.19bccb52.jpg"
+  },
+  {
+    "revision": "b988065e3b9cae9014ce7afd9aea6fdc",
+    "url": "/portfolio-2020/static/media/photo-small.b988065e.jpg"
+  },
+  {
+    "revision": "79c323dd0745d21a4a0e522fa7d8aea1",
+    "url": "/portfolio-2020/static/media/photo1.79c323dd.jpg"
+  },
+  {
+    "revision": "a8c3eaf0462e2e95e00219c2862ffcb1",
+    "url": "/portfolio-2020/static/media/photo2.a8c3eaf0.jpg"
+  },
+  {
+    "revision": "c8a183c3a32397eaff7a9fffaca4c349",
+    "url": "/portfolio-2020/static/media/phototime.c8a183c3.png"
+  },
+  {
+    "revision": "50469fdbcbede4d000e9d7c4f31c29aa",
+    "url": "/portfolio-2020/static/media/projectsPage.50469fdb.jpg"
+  },
+  {
+    "revision": "ac15f4da971107832afb5ca3692ef3bf",
+    "url": "/portfolio-2020/static/media/react-e-commerce-small.ac15f4da.jpg"
+  },
+  {
+    "revision": "9fd88c27062e3d97c2dd37a2fcfb13f7",
+    "url": "/portfolio-2020/static/media/singleProjectImage1.9fd88c27.jpg"
+  },
+  {
+    "revision": "eb3cfd1a5c63dc7aedb2acadb28d19d9",
+    "url": "/portfolio-2020/static/media/singleProjectImage3.eb3cfd1a.jpg"
+  },
+  {
+    "revision": "c30302d538a7795f5772cb1611e22b0d",
+    "url": "/portfolio-2020/static/media/slide_1.c30302d5.jpg"
+  },
+  {
+    "revision": "33503b1ae7b519a1e61423ab708e1187",
+    "url": "/portfolio-2020/static/media/slide_2.33503b1a.jpg"
+  },
+  {
+    "revision": "f62dca978b1de6daca6b5f035c7d720f",
+    "url": "/portfolio-2020/static/media/slide_3.f62dca97.jpg"
+  },
+  {
+    "revision": "21eb74bf30ef588cf89d3f18582e5c7c",
+    "url": "/portfolio-2020/static/media/stolarija-small.21eb74bf.jpg"
+  },
+  {
+    "revision": "81d77eaab5f67a36920be894e590367f",
+    "url": "/portfolio-2020/static/media/stolarija.81d77eaa.png"
+  },
+  {
+    "revision": "8db9d1b376b5b0fc579534f57c9360e2",
+    "url": "/portfolio-2020/static/media/stolarijaBatajnica.8db9d1b3.jpg"
+  },
+  {
+    "revision": "57b011c8d6f882dc1dfdda26192e2d9f",
+    "url": "/portfolio-2020/static/media/stolarijaBatajnica2.57b011c8.jpg"
+  },
+  {
+    "revision": "93255128eb2fadade81176f99cd31169",
+    "url": "/portfolio-2020/static/media/surfer-small.93255128.jpg"
+  },
+  {
+    "revision": "8084953071c336ec838e854ec973591b",
+    "url": "/portfolio-2020/static/media/surfer.80849530.png"
+  },
+  {
+    "revision": "7a4fb478de1627c046e443c5b7a2ab53",
+    "url": "/portfolio-2020/static/media/surfer1.7a4fb478.jpg"
+  },
+  {
+    "revision": "6ac909f02b5192c6283ec5ef942eccee",
+    "url": "/portfolio-2020/static/media/surfer2.6ac909f0.jpg"
+  },
+  {
+    "revision": "b1f60811412fca8562ce82841a93a84e",
+    "url": "/portfolio-2020/static/media/thunder-small.b1f60811.jpg"
+  },
+  {
+    "revision": "728d93d5a216e67169e54c61130d58c6",
+    "url": "/portfolio-2020/static/media/thunder.728d93d5.png"
+  },
+  {
+    "revision": "8d96308e21e1fae4b98f218e78f5895c",
+    "url": "/portfolio-2020/static/media/thunder1.8d96308e.jpg"
+  },
+  {
+    "revision": "d470f2decc95cafd8afcab5716551cc0",
+    "url": "/portfolio-2020/static/media/thunder2.d470f2de.jpg"
+  }
+]);
